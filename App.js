4749a387a329cddb671c02c0201b0b83f9be710a
Thujna <46659067+Soranix@users.noreply.github.com>
2024-02-18 22:39:00 +0000

    Completed second video changes (#1)

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ModuleListScreen from './src/components/screens/ModuleListScreen';
+import ModuleAddScreen from './src/components/screens/ModuleAddScreen';
+import ModuleModifyScreen from './src/components/screens/ModuleModifyScreen';
+import ModuleViewScreen from './src/components/screens/ModuleViewScreen';
+
+const Stack =createNativeStackNavigator();
 
 export const App = () => {
     //Initialisations----
@@ -7,20 +13,42 @@ export const App = () => {
     //Handlers-----------
     //View---------------
   return (
-    <View style={styles.container}>
-      <Text>I hate it here</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='ModuleViewScreen'
+        screenOptions = {{
+          headerStyle: {backgroundColor: 'black'},
+          headerTintColor: 'pink',
+        }}
+        >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen
+          name = 'ModuleListScreen'
+          component={ModuleListScreen}
+          options={{title: 'List modules'}}
+        />
+
+        <Stack.Screen
+          name = 'ModuleAddScreen'
+          component={ModuleAddScreen}
+          options={{title: 'Add Module'}}
+        />
+
+        <Stack.Screen
+          name = 'ModuleViewScreen'
+          component={ModuleViewScreen}
+          options={{title: 'View Module'}}
+        />
+
+        <Stack.Screen
+          name = 'ModuleModifyScreen'
+          component={ModuleModifyScreen}
+          options={{title: 'Modify Module'}}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;

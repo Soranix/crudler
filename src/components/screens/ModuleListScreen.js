2d17ce5b757ad00d2d4c35b4d52ca84a434b7f49
Thujna <thujna@live.co.uk>
2024-02-18 22:34:49 +0000

    Completed second video changes

---
+++ src/components/screens/ModuleListScreen.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text} from 'react-native';
+import Screen from '../layout/Screen';
+
+export const ModuleListScreen = () => {
+    //Initialisations----
+    //State--------------
+    //Handlers-----------
+    //View---------------
+  return (
+    <Screen>
+
+      <Text>List</Text>
+
+
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleListScreen;
